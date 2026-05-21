1f7c51d19ec1f70f87faf26407795260e38d5e76
Eduardo-751 <edu.s.camargo97@gmail.com>
2026-05-21 01:36:37 -0300

    Fix Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,27 +1,23 @@
 import { initializeApp } from 'firebase/app';
 
 import {
-  getFirestore,
-} from 'firebase/firestore';
-
-import {
   getAuth,
   GoogleAuthProvider,
 } from 'firebase/auth';
 
+import { getFirestore } from 'firebase/firestore';
+
 const firebaseConfig = {
-  apiKey: 'SUA_API_KEY',
-  authDomain: 'SEU_DOMINIO',
-  projectId: 'SEU_PROJECT_ID',
-  storageBucket: 'SEU_BUCKET',
-  messagingSenderId: 'SEU_ID',
-  appId: 'SEU_APP_ID',
+  apiKey: "AIzaSyDYbMe50kvGm1CvkxxKp8u9gISsJ2MzF-Y",
+  authDomain: "cha-cozinha-isabela.firebaseapp.com",
+  projectId: "cha-cozinha-isabela",
+  storageBucket: "cha-cozinha-isabela.firebasestorage.app",
+  messagingSenderId: "1005532598991",
+  appId: "1:1005532598991:web:cac75cfa6680cd1bb59dba",
+  measurementId: "G-MRE7G92408"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
-
 export const auth = getAuth(app);
-
 export const provider = new GoogleAuthProvider();
