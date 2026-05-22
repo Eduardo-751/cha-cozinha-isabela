f89d1b17a4c461285819a13072a48302afa73e70
Eduardo-751 <edu.s.camargo97@gmail.com>
2026-05-22 19:17:29 -0300

    Adicionando ScrollToTop.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import WeddingSite from './WeddingSite';
 import Presentes from './Presentes';
+import ScrollToTop from './ScrollToTop';
 
 export default function App() {
   return (
     <BrowserRouter>
+
+      <ScrollToTop />
+
       <Routes>
+        <Route path="/" element={<WeddingSite />} />
 
-        {/* Página principal */}
-        <Route
-          path="/"
-          element={<WeddingSite />}
-        />
-
-        {/* Página da lista completa */}
         <Route
           path="/presentes"
           element={<Presentes />}
         />
-
       </Routes>
+
     </BrowserRouter>
   );
 }
